89394018f994e5b4caed6f315a74ea6eabf03510
Arthur <arthur.sahakyan96@gmail.com>
2021-11-18 19:43:32 +0400

    feat(iterator): made iterator pattern

---
+++ Iterator.js
@@ -0,0 +1,16 @@
+class Iterator {
+    constructor(el) {
+        this.index = 0;
+        this.elements = el;
+    }
+
+    next() {
+        return this.elements[this.index++];
+    }
+
+    hasNext() {
+        return this.index < this.elements.length;
+    }
+}
+
+new Iterator([1, 2, 3]).next();
